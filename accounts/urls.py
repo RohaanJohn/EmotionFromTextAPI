48ef4560fee25d923a1e91fd54c195fc7b40aec1
Rohaan John <rohaanrenujohn@gmail.com>
2023-01-15 18:45:50 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("verify", views.verify, name="verify")]    
+urlpatterns = [path("analyse", views.verify, name="analyse")]    
