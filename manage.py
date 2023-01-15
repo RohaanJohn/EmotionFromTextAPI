c4f39d16fe3f35b6863529d93e4e3f9b076da481
Rohaan John <rohaanrenujohn@gmail.com>
2023-01-15 19:35:38 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,12 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import nltk
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telusko.settings')
-    nltk.download('vader_lexicon')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
